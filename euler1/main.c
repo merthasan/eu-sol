26c9806eb31175013b3d9be08524aab5e365d273
Μert <merthasan@mert-mbp.local>
2018-09-06 11:36:53 +0300

    testing

--- euler1/main.c
+++ euler1/main.c
@@ -7,9 +7,17 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main() {
+    int i = 1;
+    int sum = 0;
+    while ( i < 1000) {
+        if (i % 3 == 0)
+            sum = sum + i;
+        else if (i % 5 == 0)
+            sum = sum + i;
+        i++;
+    }
+    printf("Sum of all the multiples of 3 or 5 below 1000 is %d.\n", sum);
 }
