ed261a4ad32eca6aa99275677f8966a680b74747
Μert <merthasan@mert-mbp.local>
2018-09-06 11:28:55 +0300

    Initial Commit

---
+++ euler1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  euler1
+//
+//  Created by Mert Hasan on 6.09.2018.
+//  Copyright © 2018 Mert Hasan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
